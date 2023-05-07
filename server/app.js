7f0ef2655e5464f4010d2eb75fa6c6fb9d4de2c0
Namita Kumari <cs20btech11034@iith.ac.in>
2023-05-07 21:05:33 +0530

    removing cors access to only localhost:3000

--- server/app.js
+++ server/app.js
@@ -17,12 +17,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 
 // Available routes
